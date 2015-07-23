f0bcb05dbf1df0539e9eb398bc7f820788b14c96
Mikhail Edoshin <m.edoshin@mac.com>
2015-07-23 16:13:34 +0300

    Excluded code that cannot go into 64-bit Mac version.

--- Example/FMPlugInTahoma.cpp
+++ Example/FMPlugInTahoma.cpp
@@ -20,7 +20,6 @@
 #include "FMWrapper/FMXCalcEngine.h"
 
 #include "Support/FMPluginFunctions.h"
-#include "Support/FMPluginPrefs.h"
 
 /****************************************************************************
  * Functions                                                                *
@@ -125,149 +124,6 @@ Do_PluginShutdown2(void)
 	FMX_ExternCallPtr	gFMX_ExternCallPtr=NULL;
 
 
-
-/* ::=- Do_PluginInit =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=:: */
-static fmx::int32 Do_PluginInit(fmx::int16 version) 
-{
-	// Check the app API version
-	if ((version < k70ExtnVersion) || (version > kMaxExtnVersion)) 
-	{
-		// This version of FileMaker is not supported; let FileMaker disable this 
-		// plug-in and report the problem to the user.
-		return (kBadExtnVersion);
-	}
-
-
-	// Setup a preference file (Mac OS X) or make changes to preferences settings in the Windows Registry for the plug-in.  
-	{
-		FMPluginTahomaPrefsWidget		prefsLoader;
-		if(prefsLoader.Exists() == false)
-		{
-			prefsLoader.Create();
-		}
-
-		if(prefsLoader.Read() == false)
-		{
-			return (kDoNotEnable);
-
-		}// prefsLoader Read
-	}
-
-
-	// Register plug-in functions
-	fmx::QuadCharAutoPtr	pluginID('P', 'G', 'r', 'p');
-	fmx::TextAutoPtr		name;
-	fmx::TextAutoPtr		prototype;
-	fmx::errcode			err;
-	fmx::uint32				regFunctionFlags = fmx::ExprEnv::kMayEvaluateOnServer | fmx::ExprEnv::kDisplayInAllDialogs;
-
-	Do_GetString(kPGrp_AddStringID, name, true);
-	Do_GetString(kPGrp_AddStringID, prototype);
-	err = fmx::ExprEnv::RegisterExternalFunction(*pluginID, kPGrp_Add, *name, *prototype, 2, 2, regFunctionFlags, Do_PGrp_Add );
-	//	if (err != 0)   // You should warn the user or recover. For the purposes of this example, we ignore the result.
-
-	Do_GetString(kPGrp_LDistanceStringID, name, true);
-	Do_GetString(kPGrp_LDistanceStringID, prototype);
-	err = fmx::ExprEnv::RegisterExternalFunction(*pluginID, kPGrp_LDistance, *name, *prototype, 2, 2, regFunctionFlags, Do_PGrp_LDistance );
-
-	Do_GetString(kPGrp_SoundexAmericanStringID, name, true);
-	Do_GetString(kPGrp_SoundexAmericanStringID, prototype);
-	err = fmx::ExprEnv::RegisterExternalFunction(*pluginID, kPGrp_SoundexAmerican, *name, *prototype, 1, 1, regFunctionFlags, Do_PGrp_SoundexAmerican );
-
-	Do_GetString(kPGrp_SoundexDMStringID, name, true);
-	Do_GetString(kPGrp_SoundexDMStringID, prototype);
-	err = fmx::ExprEnv::RegisterExternalFunction(*pluginID, kPGrp_SoundexDM, *name, *prototype, 1, 1, regFunctionFlags, Do_PGrp_SoundexDM );
-
-	Do_GetString(kPGrp_ExecuteSQLStringID, name, true);
-	Do_GetString(kPGrp_ExecuteSQLStringID, prototype);
-	err = fmx::ExprEnv::RegisterExternalFunction(*pluginID, kPGrp_ExecuteSQL, *name, *prototype, 3, 3, regFunctionFlags, Do_PGrp_ExecuteSQL );
-
-	Do_GetString(kPGrp_ReduxStringID, name, true);
-	Do_GetString(kPGrp_ReduxStringID, prototype);
-	err = fmx::ExprEnv::RegisterExternalFunction(*pluginID, kPGrp_Redux, *name, *prototype, 1, 1, regFunctionFlags, Do_PGrp_Redux );
-
-	Do_GetString(kPGrp_AppendStringID, name, true);
-	Do_GetString(kPGrp_AppendStringID, prototype);
-	err = fmx::ExprEnv::RegisterExternalFunction(*pluginID, kPGrp_Append, *name, *prototype, 1, -1, regFunctionFlags, Do_PGrp_Append );
-
-	Do_GetString(kPGrp_EvaluateStringID, name, true);
-	Do_GetString(kPGrp_EvaluateStringID, prototype);
-	err = fmx::ExprEnv::RegisterExternalFunction(*pluginID, kPGrp_Evaluate, *name, *prototype, 1, 1, regFunctionFlags, Do_PGrp_Evaluate );
-
-	Do_GetString(kPGrp_StartScriptStringID, name, true);
-	Do_GetString(kPGrp_StartScriptStringID, prototype);
-	regFunctionFlags = fmx::ExprEnv::kDisplayInAllDialogs;
-	err = fmx::ExprEnv::RegisterExternalFunction(*pluginID, kPGrp_StartScript, *name, *prototype, 2, 3, regFunctionFlags, Do_PGrp_StartScript );
-
-	Do_GetString(kPGrp_UserFormatNumberStringID, name, true);
-	Do_GetString(kPGrp_UserFormatNumberStringID, prototype);
-	regFunctionFlags = fmx::ExprEnv::kDisplayCalcFields |  fmx::ExprEnv::kDisplayAutoEnter | fmx::ExprEnv::kDisplayCustomFunctions | fmx::ExprEnv::kDisplayGeneric;
-	err = fmx::ExprEnv::RegisterExternalFunction(*pluginID, kPGrp_UserFormatNumber, *name, *prototype, 1, 1, regFunctionFlags, Do_PGrp_CommonFormatNumber );
-
-	Do_GetString(kPGrp_FormatNumberStringID, name, true);
-	Do_GetString(kPGrp_FormatNumberStringID, prototype);
-	regFunctionFlags = fmx::ExprEnv::kDisplayAutoEnter;
-	err = fmx::ExprEnv::RegisterExternalFunction(*pluginID, kPGrp_FormatNumber, *name, *prototype, 2, 2, regFunctionFlags, Do_PGrp_CommonFormatNumber );
-
-	Do_GetString(kPGrp_NumToWordsStringID, name, true);
-	Do_GetString(kPGrp_NumToWordsStringID, prototype);
-	regFunctionFlags = fmx::ExprEnv::kMayEvaluateOnServer | fmx::ExprEnv::kDisplayInAllDialogs;
-	err = fmx::ExprEnv::RegisterExternalFunction(*pluginID, kPGrp_NumToWords, *name, *prototype, 1, 1, regFunctionFlags, Do_PGrp_NumToWords );
-
-	Do_GetString(kPGrp_VersionStringID, name, true);
-	Do_GetString(kPGrp_VersionStringID, prototype);
-	err = fmx::ExprEnv::RegisterExternalFunction(*pluginID, kPGrp_Version, *name, *prototype, 0, 0, regFunctionFlags, Do_PGrp_Version );
-
-
-	// Return kCurrentExtnVersion to enable the plug-in in FileMaker.
-	return (kCurrentExtnVersion);
-	
-} // Do_PluginInit
-
-
-
-static void 
-Do_PluginIdle(FMX_IdleLevel idleLevel) 
-{
-    // Check idle state
-    switch (idleLevel) {
-        case kFMXT_UserIdle:
-            // The plug-in is free to do idle processing at this time.
-            break;
-        case kFMXT_UserNotIdle:
-        case kFMXT_ScriptPaused:
-        case kFMXT_ScriptRunning:
-        case kFMXT_Unsafe:
-            // The plug-in should wait and do nothing at this time.
-            break;
-    }
-}
-
-static void 
-Do_PluginShutdown(void) 
-{
-	// Unregister plug-in functions
-	fmx::QuadCharAutoPtr	pluginID('P', 'G', 'r', 'p');
-	fmx::errcode			err;
-
-	err = fmx::ExprEnv::UnRegisterExternalFunction(*pluginID, kPGrp_Add );
-	// if (err != 0)  // There is likely nothing to do in response. For the purposes of this example, we ignore the result.
-
-	err = fmx::ExprEnv::UnRegisterExternalFunction(*pluginID, kPGrp_LDistance );
-	err = fmx::ExprEnv::UnRegisterExternalFunction(*pluginID, kPGrp_SoundexAmerican );
-	err = fmx::ExprEnv::UnRegisterExternalFunction(*pluginID, kPGrp_SoundexDM );
-	err = fmx::ExprEnv::UnRegisterExternalFunction(*pluginID, kPGrp_Append );
-	err = fmx::ExprEnv::UnRegisterExternalFunction(*pluginID, kPGrp_Evaluate );
-	err = fmx::ExprEnv::UnRegisterExternalFunction(*pluginID, kPGrp_ExecuteSQL );
-	err = fmx::ExprEnv::UnRegisterExternalFunction(*pluginID, kPGrp_Redux );
-	err = fmx::ExprEnv::UnRegisterExternalFunction(*pluginID, kPGrp_StartScript );
-	err = fmx::ExprEnv::UnRegisterExternalFunction(*pluginID, kPGrp_UserFormatNumber );
-	err = fmx::ExprEnv::UnRegisterExternalFunction(*pluginID, kPGrp_FormatNumber );
-	err = fmx::ExprEnv::UnRegisterExternalFunction(*pluginID, kPGrp_NumToWords );
-	err = fmx::ExprEnv::UnRegisterExternalFunction(*pluginID, kPGrp_Version );
-
-} // Do_PluginShutdown
-
 static const char* plugin_strings[] = {
     "PGrp"   /* Plugin-ID */
        "1"   /* fixed value */
@@ -309,14 +165,8 @@ FMExternCallProc(FMX_ExternCallPtr pb)
             gFMX_ExternCallPtr->result = Do_PluginInit2(
                     gFMX_ExternCallPtr->extnVersion);
             break;
-        case kFMXT_Idle:
-            Do_PluginIdle(gFMX_ExternCallPtr->parm1);
-            break;
         case kFMXT_Shutdown:
             Do_PluginShutdown2();
-            break;
-        case kFMXT_DoAppPreferences:
-            Do_PluginPrefs();
             break;
         case kFMXT_GetString:
             Do_GetString2(pb->parm1, pb->parm3, 
--- Makefile
+++ Makefile
@@ -103,7 +103,7 @@ build/win/64/Tahoma.fmx64: $(tahoma_win_64_o) $(tahoma_win_64_res)
 build/mac/%.o: Example/%.cpp
 	@$(MAKE-PARENT-DIRECTORY?)
 	@$(MESSAGE) "$${g}[Compiling C++]$${z} "
-	g++ $(CPPFLAGS) -arch i386 -c \
+	g++ $(CPPFLAGS) -arch i386 -arch x86_64 -c \
         -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk -mmacosx-version-min=10.4 \
         -o$@ $<
 
